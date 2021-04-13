321a5009dc993f29dd7140d071da2555e4fe7b00
sadi <acserv.informatica@yahoo.com.br>
2021-04-13 20:51:35 -0300

    novo branch infoserv

--- aula1.php
+++ aula1.php
@@ -2,9 +2,18 @@
 echo "ola mundo !!!";
 echo "<br>";
 phpinfo();
+obterDataAtual();
 echo "simulando um conflito";
 echo "criamos o branch DEV";
 /*
 o comando abaixo imprime a data
 */
 echo date("d/m/Y"); // 13/04/2021
+
+function obterDataAtual() {
+return date("d/m/Y");
+}
+function exibirDataAtual() {
+    obterDataAtual("12/04/2021");
+    obterDataAtual();
+    }
