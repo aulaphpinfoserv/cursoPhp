ce9f84e27b9baf4d15f55790febbf01ab8a46aa7
sadi <acserv.informatica@yahoo.com.br>
2021-04-13 21:03:03 -0300

    adicionamos mensagem de conflito no dev

--- aula1.php
+++ aula1.php
@@ -2,6 +2,12 @@
 echo "ola mundo !!!";
 echo "<br>";
 phpinfo();
+echo "nosso teste de conflito";
+echo "nosso teste de conflito";
+echo "nosso teste de conflito";
+echo "nosso teste de conflito";
+echo "nosso teste de conflito";
+echo "nosso teste de conflito";
 echo "simulando um conflito";
 echo "criamos o branch DEV";
->>>>>>> master
+
